531a12a79fe66753375cb74046d041f28eeb5e69
Charith <rajithacharith@gmail.com>
2018-10-10 09:12:58 +0530

    search not finished

---
+++ search.php
@@ -0,0 +1,140 @@
+<?php
+//include db_connection class
+include 'db_connection.php';
+//get a db_connection
+$conn = OpenCon();
+//start the session
+session_start();
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    if(isset($username)){
+        $sql = "SELECT * FROM student WHERE username = '$username'";
+        $results = $conn -> query($sql);
+        $count=mysqli_num_rows($results);
+    }
+    CloseCon($conn);
+}
+
+?>
+<html lang="en">
+<head>
+    <title>login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <!--===============================================================================================-->
+</head>
+<body>
+<?php
+include 'up.php';
+
+?>
+
+<div class="limiter">
+    <div class="container-login100">
+        <div class="wrap-login100">
+            <div class="login100-form-title" style="background-image: url(images/bg-01.jpg);">
+					<span class="login100-form-title-1">
+                            Search
+                    </span>
+            </div>
+
+            <form class="login100-form validate-form" action ="" method = "POST">
+                <div class="wrap-input100">
+                    <span class="label-input100">Username</span>
+                    <input class="input100" type="text" name="username" placeholder="Enter username">
+                    <span class="focus-input100"></span>
+                </div>
+                <div class="wrap-input100">
+                    <span class="label-input100">Register No</span>
+                    <input class="input100" type="password" name="password" placeholder="Enter password">
+                    <span class="focus-input100"></span>
+                </div>
+                <br><br>
+
+
+
+
+                <div class="container-login100-form-btn">
+                    <button class="login100-form-btn" type = "submit" style="margin:5%">
+                        Search
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div>
+    <table class="table table-striped">
+        <tbody>
+        <?php
+        while($row = $results->fetch_assoc()) {
+            echo '<tr><td>Username</td><td>',$row['username'],'</tr><tr><td>Password</td><td>',$row['password'],'</td></tr>';
+            //echo $row["username"],"  ", $row["password"]. "  " . $row["dob"]. "<br>";
+        }
+        ?>
+
+
+
+
+        </tbody>
+    </table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+<!--===============================================================================================-->
+<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+<script src="vendor/bootstrap/js/popper.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+<script src="vendor/daterangepicker/moment.min.js"></script>
+<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+<script src="js/main.js"></script>
+
+</body>
+</html>
