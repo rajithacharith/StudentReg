6c52392e1aad44e29fff28b7dec07319ff0d18f8
Charith <rajithacharith@gmail.com>
2018-10-24 12:27:26 +0530

    added new database

--- studentdb.sql
+++ studentdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 10, 2018 at 05:59 AM
+-- Generation Time: Oct 24, 2018 at 08:56 AM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 5.6.36
 
@@ -25,38 +25,90 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `courseID` int(11) NOT NULL,
+  `couseName` varchar(40) NOT NULL,
+  `Course_teacher` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`courseID`, `couseName`, `Course_teacher`) VALUES
+(1, 'PHP', 'no1'),
+(2, 'JAVA', 'no2');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `student`
 --
 
 CREATE TABLE `student` (
   `reg_no` int(11) NOT NULL,
-  `username` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL,
   `address` varchar(50) NOT NULL,
-  `program_of_add` enum('computer','science','','') NOT NULL,
   `date _of_add` date DEFAULT NULL,
   `NIC` varchar(11) DEFAULT NULL,
   `dob` date NOT NULL,
-  `gender` enum('male','female','','') NOT NULL
+  `gender` enum('male','female','','') NOT NULL,
+  `courseID` int(11) NOT NULL,
+  `name` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `student`
 --
 
-INSERT INTO `student` (`reg_no`, `username`, `password`, `address`, `program_of_add`, `date _of_add`, `NIC`, `dob`, `gender`) VALUES
-(1, 'rajithacharith', 'rajitha', 'adlsdvbslb', 'computer', '2018-10-18', '99999999999', '2018-06-12', 'male'),
-(2, 'jj', 'jj', 'sdfd', '', '2018-10-01', 'sdfsd', '2018-10-10', 'male');
+INSERT INTO `student` (`reg_no`, `address`, `date _of_add`, `NIC`, `dob`, `gender`, `courseID`, `name`) VALUES
+(1, 'adlsdvbslb', '2018-10-18', '99999999999', '2018-06-12', 'male', 0, ''),
+(2, 'sdfd', '2018-10-01', 'sdfsd', '2018-10-10', 'male', 0, ''),
+(5, 'Veyangoda, Western Province, Sri Lanka', '2018-10-04', '1234567890', '2018-01-02', 'male', 0, 'Charith'),
+(6, 'jvjaj', '2018-10-18', 'qeref', '2018-10-01', 'male', 1, 'Kalana');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userID` varchar(10) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userID`, `username`, `password`) VALUES
+('1', 'admin', 'e6e061838856bf47e1de730719fb2609');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`courseID`);
+
+--
 -- Indexes for table `student`
 --
 ALTER TABLE `student`
   ADD PRIMARY KEY (`reg_no`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
